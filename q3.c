e2635360aeceec82273931ff9c4142793c2a21d2
shrushti1724 <shrustipatel0817@gmail.com>
2024-11-25 16:02:05 +0530

    Add files via upload

---
+++ q3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+	cube(9);
+}
+void cube(int n)
+
+{
+	printf("%d",n*n*n);
+}
+	
+
---
+++ q4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main()
+{
+	vote(56);
+}
+void vote(int n)
+{
+	
+	
+	
+	if(n>=18)
+	{
+		printf("you are eligible for vote !");
+	}
+	else
+	{
+		printf("you are not eligible for vote !");
+	}
+	
+}
